1ff331e25a3867bb5b073216febf478d8badec73
David Surumen <davidsurumen@gmail.com>
2023-07-14 20:11:06 +0300

    edge case fix -set contains non-string values

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
 export default function cleanSet(set, startString) {
   // returns a string of all the set values that start with a specific string
-  if (typeof startString !== 'string') throw new TypeError('start string must be a string');
+  if (typeof startString !== 'string') return '';
   if (!(set instanceof Set)) throw new TypeError('set must be a Set object');
 
   const result = [];
 
   for (const val of set.values()) {
-    if (val.startsWith(startString)) {
+    if (typeof val === 'string' && val.startsWith(startString)) {
       const substr = val.substring(startString.length);
-      
+
       if (substr !== val) result.push(substr);
     }
   }
