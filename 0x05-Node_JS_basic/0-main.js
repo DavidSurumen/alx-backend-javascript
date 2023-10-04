240ecd0aac4513390e7d41fb6f8dd7f1f3b928f1
David Surumen <davidsurumen@gmail.com>
2023-10-04 17:33:04 +0300

    0 main file

--- 0x05-Node_JS_basic/0-main.js
+++ 0x05-Node_JS_basic/0-main.js
@@ -1,3 +1,5 @@
 const displayMessage = require('./0-console');
 
-displayMessage("Hello NoeJS!");
+displayMessage("Hello NodeJS!");
+displayMessage('We are going to learn Node JS today');
+displayMessage('Hello Holberton School!');
