ca8ef928390ef072310d802689e700b2802d7813
David Surumen <davidsurumen@gmail.com>
2023-11-24 14:06:52 +0300

    task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+function countStudents (path) {
+  // data format: FIRSTNAME, LASTNAME, AGE, FIELD
+  fs.readFile(path, 'utf-8', (error, data) => {
+    if (error) {
+      throw new Error('Cannot load the database');
+    }
+
+    // by default readFile reads content of a file as a string
+    // ...extract into an array...
+    let results = [];
+    const lines = data.split('\n'); // the strings in data are delimitted with \n
+    // split into rows of data
+    lines.forEach((line) => {
+      const values = line.split(',');
+      results.push(values);
+    });
+    // first item is label for columns, and the last is a null character. exclude
+    results = results.slice(1, -1);
+    console.log('Number of students: ' + results.length);
+
+    // create list of objects to represent each field, and the students
+    // in that field.
+    const fields = [];
+    for (let i = 0; i < results.length; i++) {
+      const fieldName = results[i][results[i].length - 1];
+      // check if field already added to the fields list
+      const findField = fields.find(field => field.fieldName === fieldName);
+
+      if (findField) {
+        findField.count++;
+        findField.firstnames.push(results[i][0]);
+      } else {
+        fields.push({
+          fieldName: fieldName,
+          count: 1,
+          firstnames: [results[i][0]]
+        });
+      }
+    }
+
+    for (const itm in fields) {
+      console.log('Number of students in ' +
+                  fields[itm].fieldName + ': ' +
+                  fields[itm].count +
+                  '. List: ' + fields[itm].firstnames.join(', '));
+    }
+  });
+}
+
+module.exports = countStudents;
