8eabdad3a8ec387b03336591270971c915c687dc
David Surumen <davidsurumen@gmail.com>
2023-11-25 00:26:38 +0300

    task 3 -async read

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,33 +1,37 @@
 const fs = require('node:fs/promises');
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, { encoding: 'utf-8' });
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' })
+      .then((data) => {
+        // split into rows, and exclude the data header
+        const results = data.trim().split('\n').map((row) => row.split(',')).slice(1);
+        console.log(`Number of students: ${results.length}`);
 
-    // split into rows, and exclude the data header
-    const results = data.trim().split('\n').map((row) => row.split(',')).slice(1);
-    console.log(`Number of students: ${results.length}`);
+        const fieldCounts = {};
+        const fieldStudents = {};
 
-    const fieldCounts = {};
-    const fieldStudents = {};
+        /* eslint-disable no-unused-vars */
+        results.forEach((student) => {
+          const [firstname, lastname, age, field] = student;
+          fieldCounts[field] = (fieldCounts[field] || 0) + 1;
+          fieldStudents[field] = fieldStudents[field] || [];
+          fieldStudents[field].push(firstname);
+        });
+        /* eslint-enable no-unused-vars */
 
-    /* eslint-disable no-unused-vars */
-    results.forEach((student) => {
-      const [firstname, lastname, age, field] = student;
-      fieldCounts[field] = (fieldCounts[field] || 0) + 1;
-      fieldStudents[field] = fieldStudents[field] || [];
-      fieldStudents[field].push(firstname);
-    });
-    /* eslint-enable no-unused-vars */
+        Object.keys(fieldCounts).forEach((field) => {
+          const count = fieldCounts[field];
+          const studentsList = fieldStudents[field].join(', ');
+          console.log(`Number of students in ${field}: ${count}. List: ${studentsList}`);
+        });
 
-    Object.keys(fieldCounts).forEach((field) => {
-      const count = fieldCounts[field];
-      const studentsList = fieldStudents[field].join(', ');
-      console.log(`Number of students in ${field}: ${count}. List: ${studentsList}`);
-    });
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+        resolve(); // resolve the promise when processing is complete
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
+  });
 }
 
 module.exports = countStudents;
