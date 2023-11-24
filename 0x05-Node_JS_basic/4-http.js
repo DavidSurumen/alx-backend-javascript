016ba7fa9e9dc84fefdf8facb3bf451cc5867969
David Surumen <davidsurumen@gmail.com>
2023-11-25 01:07:02 +0300

    style fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 const app = require('http');
 
-app.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+app.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
   response.end('Hello Holberton School!');
 }).listen(1245);
 
