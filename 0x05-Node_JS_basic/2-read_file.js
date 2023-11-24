89a8204801d91a8304adc33b633d734bd42430d3
David Surumen <davidsurumen@gmail.com>
2023-11-24 17:05:41 +0300

    elint fix for unused vars

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,21 +3,23 @@ const fs = require('fs');
 function countStudents(path) {
   // data format: FIRSTNAME, LASTNAME, AGE, FIELD
   try {
-    const data = fs.readFileSync(path, 'utf-8');  // read in string format
+    const data = fs.readFileSync(path, 'utf-8'); // read in string format
 
     // split data into rows.
-    const results = data.trim().split('\n').map(row => row.split(',')).slice(1);
+    const results = data.trim().split('\n').map((row) => row.split(',')).slice(1);
     console.log(`Number of students: ${results.length}`);
 
     const fieldCounts = {};
     const fieldStudents = {};
 
-    results.forEach(studt => {
+    /* eslint-disable no-unused-vars */
+    results.forEach((studt) => {
       const [firstname, lastname, age, field] = studt;
       fieldCounts[field] = (fieldCounts[field] || 0) + 1;
       fieldStudents[field] = fieldStudents[field] || [];
       fieldStudents[field].push(firstname);
     });
+    /* eslint-enable no-unused-vars */
 
     Object.keys(fieldCounts).forEach((field) => {
       const count = fieldCounts[field];
