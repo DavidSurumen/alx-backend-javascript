f1475fcd3ab468c684657ad9993ea97c0e2d92ba
David Surumen <davidsurumen@gmail.com>
2023-11-25 01:15:38 +0300

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,17 @@
-const app = require('http');
+const http = require('http');
 
-app.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello Holberton School!');
-}).listen(1245);
+const PORT = 1245;
+const app = http.createServer();
+
+app.on('request', (_, response) => {
+  const text = 'Hello Holberton School!';
+
+  response.setHeader('Content-Type', 'text/plain');
+  response.setHeader('Content-Length', text.length);
+  response.statusCode = 200;
+  response.write(Buffer.from(text));
+});
+
+app.listen(PORT);
 
 module.exports = app;
