7b28470cb70457d4178974b19d06bc091e3ceae0
David Surumen <davidsurumen@gmail.com>
2023-11-25 01:00:09 +0300

    task 4 -small http server using node http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,8 @@
+const app = require('http');
+
+app.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello Holberton School!');
+}).listen(1245);
+
+module.exports = app;
