4222aa7993823121bff0500af577fa8f0ffc9793
davidsurumen@gmail.com <davidsurumen@gmail.com>
2024-05-03 19:03:11 +0300

    improved test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,16 +4,13 @@ import calculateNumber from './0-calcul.js';
 import assert from 'assert';
 
 describe('#calculateNumber', function() {
-    it('should return 9 given 3.6 and 4.7', function() {
-        assert.equal(calculateNumber(3.6, 4.7), 9);
+    it('should return 4 given 1.4 and 3 -first number rounded', () => {
+        assert.equal(calculateNumber(1.4, 3), 4);
     });
-    it('should return 5 given 2.4 and 3.2', () => {
-        assert.equal(calculateNumber(2.4, 3.2), 5);
+    it('should return 6 given 2 and 3.6 -second number rounded', () => {
+        assert.equal(calculateNumber(2, 3.6), 6);
     });
-    it('should return 7 given 5 and 1.6', () => {
-        assert.equal(calculateNumber(5, 1.6), 7);
-    });
-    it('should return 4 given 1 and 3', () => {
-        assert.equal(calculateNumber(1, 3), 4);
+    it('should return 9 given 3.5 and 4.7 -both numbers rounded', () => {
+        assert.equal(calculateNumber(3.5, 4.7), 9);
     });
 });
