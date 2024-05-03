4a6d42dca5c6ae0df2752d8e2010d7b8c2db3c9d
davidsurumen@gmail.com <davidsurumen@gmail.com>
2024-05-03 19:18:06 +0300

    improved test suite for second number rounded

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,8 +5,8 @@ describe('#calculateNumber', function() {
     it('should return 4 given 1.4 and 3 -first number rounded', () => {
         assert.equal(calculateNumber(1.4, 3), 4);
     });
-    it('should return 6 given 2 and 3.6 -second number rounded', () => {
-        assert.equal(calculateNumber(2, 3.6), 6);
+    it('should return 5 given 3 and 1.4 -rounding up second number', () => {
+        assert.equal(calculateNumber(3, 1.534321), 5);
     });
     it('should return 9 given 3.5 and 4.7 -both numbers rounded', () => {
         assert.equal(calculateNumber(3.5, 4.7), 9);
