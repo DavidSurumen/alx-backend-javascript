71763d1f1d2f5e424baa55c602dd70206d12195f
davidsurumen@gmail.com <davidsurumen@gmail.com>
2024-05-05 11:55:00 +0300

    added tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,13 +2,23 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul'); 
 
 describe('#calculateNumber', function() {
-    it('should return 4 given 1.4 and 3 -first number rounded', () => {
-        assert.equal(calculateNumber(1.4, 3), 4);
+    it('should round the first number', () => {
+        assert.equal(calculateNumber(3.0, 0), 3);
+        assert.equal(calculateNumber(3.4, 0), 3);
+        assert.equal(calculateNumber(3.8, 0), 4);
     });
-    it('should return 5 given 3 and 1.4 -rounding up second number', () => {
-        assert.equal(calculateNumber(3, 1.534321), 5);
+    it('should round the second number', () => {
+        assert.equal(calculateNumber(0, 1.0), 1);
+        assert.equal(calculateNumber(0, 2.3), 2);
+        assert.equal(calculateNumber(0, 4.6), 5);
+
     });
-    it('should return 9 given 3.5 and 4.7 -both numbers rounded', () => {
+    it('should return the right result', () => {
+        assert.equal(calculateNumber(1.4, 0), 1);
+        assert.equal(calculateNumber(0, 2.1), 2);
+        assert.equal(calculateNumber(1.3, 2.4), 3);
+        assert.equal(calculateNumber(2.2, 3.5), 6);
+        assert.equal(calculateNumber(2.8, 1.4), 4);
         assert.equal(calculateNumber(3.5, 4.7), 9);
     });
 });
